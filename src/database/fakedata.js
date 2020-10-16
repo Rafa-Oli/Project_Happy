894a62e6675b17f3a7212d501b19ca7a5aca0d6d
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-10-15 22:22:13 -0300

    js

---
+++ src/database/fakedata.js
@@ -0,0 +1,30 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-7.2290248",
+        lng: "-35.902706",
+        name: "Lar das meninas",
+        description: "sfsdfgsgf",
+        images:[
+    "https://images.unsplash.com/photo-1600711725407-2ea4733a38c3?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+   "https://images.unsplash.com/photo-1602571833724-984f81111ce1?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "sjfsajfjdfnsdngvjsodg",
+        opening_hours:"dsfsdgd",
+        open_on_weekends:"1"
+    },
+    {
+        id: 2,
+        lat: "-7.2290248",
+        lng: "-35.903706",
+        name: "Lar das meninos",
+        description: "sfsdfgsgf",
+        images:[
+            "https://images.unsplash.com/photo-1600711725407-2ea4733a38c3?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+           "https://images.unsplash.com/photo-1602571833724-984f81111ce1?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+                ],
+        instructions: "sjfsajfjdfnsdngvjsodg",
+        opening_hours:"dsfsdgd",
+        open_on_weekends:"0"
+    }
+]
--- src/server.js
+++ src/server.js
@@ -1,11 +1,23 @@
 //importar dependencia
 const express = require('express')
+const path= require('path')
+const pages= require('./pages.js')
 
 //iniciando o express
 const server = express()
 
-//criar uma rota
-server.get('/', () => {})
+server.use(express.static('public')) //utilizando os arquivos estáticos
+//configurar template engine
+.set('views', path.join(__dirname, "views"))
+.set('view engine', 'hbs')
+
+
+
+//rotas da aplicação
+.get('/',pages.index)
+.get('/orphanage',pages.orphanage)
+.get('/orphanages',pages.orphanages)
+.get('/create-orphanage',pages.createOrphanage)
 
 //ligar o servidor
 server.listen(5500)
