e9100f5ba7ff0a69d7a35f353c50888a2cccdf0f
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-10-16 16:18:48 -0300

    js

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -11,16 +11,32 @@ const icon = L.icon({
     iconAnchor: [29,68],
     popupAnchor: [170,2]
 })
+
+function addMarker({id,name,lat,lng}){
+
 //create popup overlay
 const popup = L.popup({
     closeButton: false,
     className: 'map-popup',
     minWidth: 240,
     minHeight: 240
-}).setContent('Lar das meninas <a href= "orphanage?id=1" class="choose-orphanages"> <img src="/images/arrow-white.svg" ></a>')
+}).setContent(`${name} <a href= "orphanage?id=${id}"><img src="/images/arrow-white.svg" ></a>`)
 
 //create and add marker
-L.marker([-7.2290248,-35.902706], {icon})
+L.marker([lat,lng], {icon})
     .addTo(map)
     .bindPopup(popup)
+}
+
+const orphanagesSpan= document.querySelectorAll('.orphanages span')
+orphanagesSpan.forEach( span => {
+    const orphanage = {
+        id: span.dataset.id, 
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng
+    }
+    addMarker(orphanage)
+})
+
    
