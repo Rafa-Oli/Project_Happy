3cb0f53415f233d704dc2a8d548a8d735128c3be
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-10-16 21:06:21 -0300

    js

--- src/database/test.js
+++ src/database/test.js
@@ -1,30 +1,22 @@
 const Database= require('./db')
-
+const saveOrphanage= require('./saveOrphanage')
 Database.then(async db => {
+
+
 //inserir dados na tabela
-await db.run(`
-INSERT INTO orphanages(
-    lat,
-    lng,
-    name,
-    about,
-    whatsapp,
-    images,
-    instructions,
-    opening_hours,
-    open_on_weekends
-)VALUES(
-    "-7.2290248",
-    "-35.902706",
-    "Lar das meninas",
-    "sfsdfgsgf",
-    "514544151",
-    "https://images.unsplash.com/photo-1602571833724-984f81111ce1?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-    "sjfsajfjdfnsdngvjsodg",
-    "dsfsdgd",
-    "1"
-); 
-`)
+await saveOrphanage(db,{ 
+    lat: "-7.2290248",
+    lng: "-35.903706",
+    name: "Lar de amor",
+    description: "sfsdfgsgf",
+    images:[
+"https://images.unsplash.com/photo-1600711725407-2ea4733a38c3?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+"https://images.unsplash.com/photo-1602571833724-984f81111ce1?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+    ].toString,
+    instructions: "sjfsajfjdfnsdngvjsodg",
+    opening_hours:"dsfsdgd",
+    open_on_weekends:"1"
+})
 
 
 //consultar dados da tabela
@@ -33,6 +25,6 @@ console.log(selectedOrphanage)
 
 
 //consultar somente 1 orphanato, pel id
-const orphanage= await db.all("SELECT * FROM orphanages where id = '1' " )
+const orphanage= await db.all("SELECT * FROM orphanages where id = '3' " )
 console.log(orphanage)
 })
